e696f40a0255b6c505b7914fed70ace970ea719d
bharath02 <fee0228@gmail.com>
2020-02-21 18:58:45 +0530

    unorder linked list values to solve

---
+++ DataStructures/1.0_unorderList.py
@@ -0,0 +1,81 @@
+# UnOrdered List
+# Desc -> Read the Text from a file, split it into words and arrange it as Linked List. Take a user input to search a Word in the List. If the Word is not found then add it to the list, and if it found then remove the word from the List. In the end save the list into a file
+# I/P -> Read from file the list of Words and take user input to search a Text
+# Logic -> Create a Unordered Linked List. The Basic Building Block is the Node Object. Each node object must hold at least two pieces of information. One ref to the data field and  second the ref to the next node object.
+# O/P -> The List of Words to a File.
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+temp = Node(93)
+print(temp.getData())
+
+temp1=UnorderedList()
+print(temp1.isEmpty())
+temp1.add(20)
+temp1.add(31)
+temp1.add(28)
+temp1.add(96)
+temp1.add(80)
+print(temp1.search(96))
+print(temp1.remove(31))
